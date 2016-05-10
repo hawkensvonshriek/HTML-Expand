1a61678cca9aa057683a5d3c2aed262c3361d54b
hawkensvonshriek <hawkensvonshriek@gmail.com>
2016-05-10 15:04:42 -0600

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
 #include "Frame.h"
 #include "ConfParse.h"
 
-/*
-* I commented out this (fully functional) code for accepting cmd args for ease of grading;
-* this way you don't have to specify the correct config file, HTML repository, output dir,
-* and defaults configuration file
-*/
 //conf.json /path/to/HTML/repo/ /path/to/output/site/ defaults.json (optional)
 int main(int argc, char * argv[]) {
 	if (argc < 4) {
@@ -15,8 +10,5 @@ int main(int argc, char * argv[]) {
 	} else if (argc == 5) {
 		ConfParse c(argv[1], argv[2], argv[3], argv[4]);
 	}
-
-	//Yep this is all main.cpp does--check out my whacky Frame and ConfParse classes :)
-	//ConfParse c("struct.json", "Modules/", "FinalSite/", "defaults.json");
 	return 0;
 }
